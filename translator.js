aec19c7d81f09206ae9ebfe780c08b7f60dabb4d
jtuluve <74163310+jtuluve@users.noreply.github.com>
2022-10-03 17:21:24 +0530

    Update translator.js

--- translator.js
+++ translator.js
@@ -1,6 +1,6 @@
 document.getElementById("txt").addEventListener("input", translate);
 function translate(){
     let txt = document.getElementById("txt").value;
-    let res = txt.replace(/ನಾ/g, "b");
+    let res = txt.replace(/ನ/g, "b");
     document.getElementById("res").value = res;
 }
