8494091e33fd454a02e53f5efc653ee27059b772
jtuluve <74163310+jtuluve@users.noreply.github.com>
2022-10-03 17:14:01 +0530

    Add files via upload

---
+++ translator.js
@@ -0,0 +1,6 @@
+document.getElementById("txt").addEventListener("input", translate);
+function translate(){
+    let txt = document.getElementById("txt").value;
+    let res = txt.replace(/a/g, "b");
+    document.getElementById("res").value = res;
+}
