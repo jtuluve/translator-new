e4082f235f6ab5ece37d6b46d37401b2ca0160a8
jtuluve <74163310+jtuluve@users.noreply.github.com>
2022-10-03 17:31:37 +0530

    Update translator.js

--- translator.js
+++ translator.js
@@ -1,6 +1,6 @@
 document.getElementById("txt").addEventListener("input", translate);
 function translate(){
     let txt = document.getElementById("txt").value;
-    let res = txt.replace(/ನಾನು/g, "ಯಾನ್").replace(/ನಾ/g, "c");
+    let res = txt.replace(/ನಾನು/g, "ಯಾನ್").replace(/ನೀವು/g, "ಈರ್").replace(/ಆಯೆ/g, "ಅವನು").replace(/ಮನೆ/g, "ಇಲ್ಲ").replace(/ಬೇರೆ/g, "ಬೇತೆ").replace(/ಭಾಷೆ/g, "ಬಾಸೆ");
     document.getElementById("res").value = res;
 }
