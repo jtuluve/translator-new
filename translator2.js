d75056834a7af931d7617c922095cfa9ff7a28ed
jtuluve <74163310+jtuluve@users.noreply.github.com>
2022-10-15 10:31:37 +0530

    Update translator2.js

--- translator2.js
+++ translator2.js
@@ -2,7 +2,7 @@ let kannada = ["","","","","","","","","","","",""];
 let tulu = ["", "","","","","","","","","","",""];
 let word = "";
 function(){
-for (let i = 0; i < words.length; i++) {
+for (let i = 0; i < tulu.length; i++) {
   word = txt.indexOf(kannada[i]);
   while(word>-1){
     txt = txt.replace(kannada[i], tulu[i]);
