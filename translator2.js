40876f7499711051df453ac79a5861385cf47918
jtuluve <74163310+jtuluve@users.noreply.github.com>
2022-10-15 10:42:41 +0530

    Update translator2.js

--- translator2.js
+++ translator2.js
@@ -3,10 +3,11 @@ let tulu = ["ಯಾನ್", "ಆಯೆ","ಅವ್","ಆಯೇನ","ಆಲ್","
 let word = "";
 document.getElementById("txt").addEventListener("input", new);
 function new(){
-for (let i = 0; i < tulu.length; i++) {
+for (let i = 0; i < tulu.length; i++){
   word = txt.indexOf(kannada[i]);
   while(word>-1){
     txt = txt.replace(kannada[i], tulu[i]);
+    word = txt.indexOf(kannada[i]);
   }
     document.getElementById("res").value = txt;
 }
